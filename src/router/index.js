a02f306f566e3d640a1a77552869bcb8c562cf5d
kameush <nico9017@gmail.com>
2018-03-23 17:06:32 +0100

    Switching branches

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,13 @@ import Welcome from '@/components/Welcome'
 import GreetingLogo from '@/components/GreetingLogo'
 import Dashboard from '@/components/Dashboard'
 import Navigation from '@/components/Navigation'
+import Data from '@/components/Data'
+
+// Previews
+import DBPreview from '@/components/previews/DBPreview'
+
+// Form Components
+import AddItem from '@/components/forms/AddItem'
 
 Vue.use(Router)
 
@@ -30,6 +37,19 @@ export default new Router({
           components: {
             GreetingLogo: GreetingLogo,
             Navigation: Navigation
+          }
+        }
+      ]
+    },
+    {
+      path: '/data/add',
+      component: Data,
+      children: [
+        {
+          path: '',
+          components: {
+            AddItem: AddItem,
+            DBPreview: DBPreview
           }
         }
       ]
